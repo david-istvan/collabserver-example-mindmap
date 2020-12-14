9cf353cb4a50913878006dda3a0d5f08c019851c
david-istvan <goerenh@gmail.com>
2020-12-14 04:33:42 -0800

    Init mockup

---
+++ src/client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Mindmap.cpp"
+ using namespace std;
+ 
+int main() {    
+	cout << "Hello world" << endl;
+	
+	Mindmap m ("m1");
+	cout << "m name: " << m.GetName() << endl;
+
+	
+    return 0;
+}
