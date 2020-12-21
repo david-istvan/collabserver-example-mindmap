d450b2407a90fe590ee7411e96eca365525a9f5b
david-istvan <goerenh@gmail.com>
2020-12-21 01:01:14 -0800

    Updated CREATE command

--- src/client.cpp
+++ src/client.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     /**
      *  Command format:
-     *  * CREATE [type] [name]
+     *  * CREATE [logicalTypeName] [domainTypeName] [name]
      *  * READ
      *  * UPDATE [name] [property] [value]
      *  * DELETE [name]
@@ -15,8 +15,11 @@ int main() {
     string command;
     string operation;
     string args;
-    string type;
+	string what;
+    string logicalTypeName;
+	string domainTypeName;
     string name;
+	string name2;
     string property;
     string value;
 
@@ -31,13 +34,31 @@ int main() {
 
         if (operation == "create") {
             pos = args.find(' ', 0);
-            type = args.substr(0, pos);
+            what = args.substr(0, pos);
             args = args.substr(pos + 1);
+			
+			if(what=="association"){
+				pos = args.find(' ', 0);
+				domainTypeName = args.substr(0, pos);
+				args = args.substr(pos + 1);
+				
+				pos = args.find(' ', 0);
+				name = args.substr(0, pos);
+				args = args.substr(pos + 1);
+				
+				pos = args.find(' ', 0);
+				name2 = args.substr(0, pos);
 
-            pos = args.find(' ', 0);
-            name = args.substr(0, pos);
-
-            cout << "...creation of " << name << " of type " << type << endl;
+				cout << "...creation of association  of type " << domainTypeName << " between " << name << " and " << name2 << endl;
+			}else{
+				domainTypeName = what;
+				
+				pos = args.find(' ', 0);
+				name = args.substr(0, pos);
+				args = args.substr(pos + 1);
+				
+				cout << "...creation of entity of type " << domainTypeName << " named " << name << endl;
+			}
         } else if (operation == "read") {
             cout << "...reading mindmap" << endl;
         } else if (operation == "update") {
